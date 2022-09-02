7605f6eda360aae52107a98339391d57a59b7a8f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-02 23:23:04 +0900

    #76 Setting up categories 3

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
         <div className="categories-container">
             {categories.map(({title}) => (
                 <div className="category-container">
-                    {/*<img src="" alt=""/>*/}
+                    <div className="background-image"/>
                     <div className="category-body-container">
                         <h2>{title}</h2>
                         <p>Shop Now</p>
