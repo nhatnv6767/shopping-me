6ed56e699fc5fc254b8cbf71d4164410d02d7330
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-03 00:03:29 +0900

    #78 Category item component 2

--- src/App.js
+++ src/App.js
@@ -32,9 +32,14 @@ const App = () => {
 
     return (
         <div className="categories-container">
-            {categories.map(({title, id}) => (
+            {categories.map(({title, id, imageUrl}) => (
                 <div className="category-container" key={id}>
-                    <div className="background-image"/>
+                    <div
+                        className="background-image"
+                        style={{
+                            backgroundImage: `url(${imageUrl})`
+                        }}
+                    />
                     <div className="category-body-container">
                         <h2>{title}</h2>
                         <p>Shop Now</p>
