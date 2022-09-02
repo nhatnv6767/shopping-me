5d665986aab0a7c979979d5814ad2c82071f1e58
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-02 23:19:57 +0900

    #76 Setting up categories 2

--- src/App.js
+++ src/App.js
@@ -4,16 +4,32 @@ const App = () => {
         {
             id: 1,
             title: "Hats",
-        }
+        },
+        {
+            id: 2,
+            title: "Jackets",
+        },
+        {
+            id: 3,
+            title: "Sneakers",
+        },
+        {
+            id: 4,
+            title: "Womens",
+        },
+        {
+            id: 5,
+            title: "Mens",
+        },
     ]
 
     return (
         <div className="categories-container">
-            {categories.map((category) => (
+            {categories.map(({title}) => (
                 <div className="category-container">
                     {/*<img src="" alt=""/>*/}
                     <div className="category-body-container">
-                        <h2>Hats</h2>
+                        <h2>{title}</h2>
                         <p>Shop Now</p>
                     </div>
                 </div>
