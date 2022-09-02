7a7fea063f38dc31a782b7d68e31e18ea070c2a2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-03 00:11:02 +0900

    #78 Category item component 4

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,9 +1,19 @@
 import "./category-item.styles.scss"
 
-const CategoryItem = () => {
+const CategoryItem = ({category}) => {
+    const {imageUrl, title} = category;
     return (
-        <div>
-
+        <div className="category-container">
+            <div
+                className="background-image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+            <div className="category-body-container">
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+            </div>
         </div>
     );
 };
