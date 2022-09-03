7789112a013292757410da458c31fa7da740f76a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-04 00:09:23 +0900

    #85 Navigation Bar Component 2

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
     return (
         <Routes>
             <Route path="/" element={<Navigation/>}>
-                <Route path="home" element={<Home/>}/>
+                <Route index element={<Home/>}/>
                 <Route path="shop" element={<Shop/>}/>
             </Route>
         </Routes>
