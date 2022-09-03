875393707fbb8af446d2bd8f954b0a722be869a8
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-03 23:45:35 +0900

    #84 React Router Outlet 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Home from "./routes/home/home.component";
 import {Routes, Route} from "react-router-dom"
 
+const Shop = () => {
+    return (
+        <h1>I am the shop page</h1>
+    )
+}
+
 const App = () => {
 
     return (
         <Routes>
-            <Route path="/home" element={<Home/>}/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/shop" element={<Shop/>}/>
         </Routes>
     );
 }
