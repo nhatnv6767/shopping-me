2266dcbf92c20f739edbcbb69412954462d60ab7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-03 23:32:24 +0900

    #82 Setting Homepage 4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
 
     return (
         <Routes>
-            <Route path="/" element={<Home/>}/>
+            <Route path="/home" element={<Home/>}/>
         </Routes>
     );
 }
