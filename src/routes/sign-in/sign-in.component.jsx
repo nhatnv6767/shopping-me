8a126f118925a09847047a57dddb6359444e9906
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-04 17:39:58 +0900

    #92 Authenticating with firebase 3

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,16 @@
+import {signInWithGooglePopup} from "../../utils/firebase/firebase.utils";
+
 export default function SignIn() {
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+        console.log(response)
+    }
     return (
         <div>
             <h1>Sign In Page</h1>
+            <button onClick={logGoogleUser}>
+                Sign In with Google Popup
+            </button>
         </div>
     );
 }
