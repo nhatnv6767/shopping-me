39dd60557359eda5d7f587d307bb5d613ef544cf
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-04 21:44:51 +0900

    #100 Generalizing form input component 6

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,16 +1,16 @@
 import "./form-input.styles.scss"
 
-export default function FormInput({label, ...otherProps}) {
+export default function FormInput({label, inputOptions}) {
     return (
         <div className="group">
             <input
                 className="form-input"
-                {...otherProps}
+                {...inputOptions}
             />
             {
                 label && (
                     <label
-                        className={`${otherProps.value.length ? "shrink" : ""} form-input-label`}
+                        className={`${inputOptions.value.length ? "shrink" : ""} form-input-label`}
                     >
                         {label}
                     </label>
