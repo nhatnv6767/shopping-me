64f29da5d0fca67655b4500fa85e8120660ca892
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-04 18:06:50 +0900

    #93 Setting up user documents 3

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,9 @@
-import {signInWithGooglePopup} from "../../utils/firebase/firebase.utils";
+import {signInWithGooglePopup, createUserDocumentFromAuth} from "../../utils/firebase/firebase.utils";
 
 export default function SignIn() {
     const logGoogleUser = async () => {
-        const response = await signInWithGooglePopup();
-        console.log(response)
+        const {user} = await signInWithGooglePopup();
+        await createUserDocumentFromAuth(user)
     }
     return (
         <div>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -36,5 +36,6 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
 export const db = getFirestore()
 
 export const createUserDocumentFromAuth = async (userAuth) => {
-    
+    const userDocRef = doc(db, "users", userAuth.uid)
+    console.log(userDocRef)
 }
