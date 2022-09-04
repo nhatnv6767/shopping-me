361dd99fe1b24ce303c5edf70f6126cfdc594db8
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-04 18:10:21 +0900

    #93 Setting up user documents 3.1

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -38,4 +38,7 @@ export const db = getFirestore()
 export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, "users", userAuth.uid)
     console.log(userDocRef)
+
+    const userSnapshot = await getDoc(userDocRef)
+    console.log(userSnapshot)
 }
