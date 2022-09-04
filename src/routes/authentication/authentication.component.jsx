01ad0315b96292077d6247085c9b9d5f4387ce83
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-04 22:36:27 +0900

    #102 Sign in Form 6

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -3,6 +3,7 @@ import {
     createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
 const Authentication = () => {
 
@@ -14,9 +15,7 @@ const Authentication = () => {
     return (
         <div>
             <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>
-                Sign In with Google Popup
-            </button>
+            <SignInForm/>
             <SignUpForm/>
         </div>
     );
