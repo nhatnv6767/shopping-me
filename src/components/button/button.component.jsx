c18da7a30f23df0d8da2cee11e052d9ed446b0ad
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-04 21:58:40 +0900

    #101 Custom button component 3

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,6 +1,23 @@
-export default function Button({children}) {
+/*
+default
+
+inverted
+
+google sign in
+
+ */
+
+const BUTTON_TYPES_CLASSES = {
+    google: "google-sign-in",
+    inverted: "inverted"
+}
+
+export default function Button({children, buttonType, ...otherProps}) {
     return (
-        <button className="button-container">
+        <button
+            className={`button-container ${BUTTON_TYPES_CLASSES[buttonType]}`}
+            {...otherProps}
+        >
             {children}
         </button>
     );
