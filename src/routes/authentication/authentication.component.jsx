67225139a3a953be0ea725e44b0623683e2b8468
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-04 22:27:59 +0900

    #102 Sign in Form 1

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -4,7 +4,7 @@ import {
 } from "../../utils/firebase/firebase.utils";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
-const SignIn = () => {
+const Authentication = () => {
 
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
@@ -22,4 +22,4 @@ const SignIn = () => {
     );
 }
 
-export default SignIn;
+export default Authentication;
