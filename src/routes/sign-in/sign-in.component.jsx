29cf6f02a715907a1b35d3331e623fd6f7a21cd4
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-04 18:53:41 +0900

    #96 Sign in with redirect 4

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -12,7 +12,9 @@ const SignIn = () => {
     useEffect(() => {
         async function getResponse() {
             const response = await getRedirectResult(auth)
-            console.log(response)
+            if (response) {
+                const userDocRef = await createUserDocumentFromAuth(response.user)
+            }
         }
 
         getResponse()
