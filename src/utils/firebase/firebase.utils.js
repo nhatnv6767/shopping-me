d329c36aae42f37f2f97b608435be05ec4fcb8bb
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-04 17:29:06 +0900

    #92 Authenticating with firebase 1

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,14 @@
+import {initializeApp} from "firebase/app"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDV0pXrWu9CeqFLoZ8CPWg4pfHo5yiv_r8",
+    authDomain: "crwn-clothing-db-138e7.firebaseapp.com",
+    projectId: "crwn-clothing-db-138e7",
+    storageBucket: "crwn-clothing-db-138e7.appspot.com",
+    messagingSenderId: "90493399092",
+    appId: "1:90493399092:web:983ee0cf3892a4db18da61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
