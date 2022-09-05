0f40bbfbf4e998674abf4cc17b3ff024544060bf
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-06 01:33:22 +0900

    #109 Observer: onAuthStateChange 2

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
-import {createContext, useState} from "react";
+import {createContext, useEffect, useState} from "react";
+import {onAuthStateChangedListener} from "../utils/firebase/firebase.utils";
 
 // as the actual value you want to access
 export const UserContext = createContext({
@@ -9,5 +10,13 @@ export const UserContext = createContext({
 export const UserProvider = ({children}) => {
     const [currentUser, setCurrentUser] = useState(null)
     const value = {currentUser, setCurrentUser}
+
+    useEffect(() => {
+        const unsubcribe = onAuthStateChangedListener((user) => {
+            console.log(user)
+        })
+        return unsubcribe
+    }, [])
+
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
