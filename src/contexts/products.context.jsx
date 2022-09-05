ff73d9152f5c232eb040b8a7da63bb24f6e34064
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-06 02:23:20 +0900

    #113 Products context 2

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,11 +1,16 @@
-import {createContext} from "react";
+import {createContext, useState} from "react";
 
 import PRODUCTS from "../shop-data.json"
 
-export const ProductContext = createContext({})
+export const ProductContext = createContext({
+    products: [],
+})
 
 export const ProductsProvider = ({children}) => {
+    const [products, setProducts] = useState(PRODUCTS)
+    /* Creating a new object with the key of products and the value of products. */
+    const value = {products}
     return (
-        <ProductContext.Provider> {children} </ProductContext.Provider>
+        <ProductContext.Provider value={products}>{children}</ProductContext.Provider>
     )
 }
