31aed11aa2ebdb4bde162776d3b89e8f03ba69f7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-05 22:15:08 +0900

    #105 User context 3

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import {BrowserRouter as Router} from "react-router-dom"
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {UserProvider} from "./contexts/user.context";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Router>
-            <App/>
+            <UserProvider>
+                <App/>
+            </UserProvider>
         </Router>
     </React.StrictMode>
 );
