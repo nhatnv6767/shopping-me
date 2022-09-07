54cc034d7fad6c7384fadcd95d089f68ea04f804
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-07 22:26:17 +0900

    #117 Add to Cart 6

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -2,10 +2,21 @@ import {createContext, useState} from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
     // find if cartItems contains productToAdd
+    const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id)
 
     // if found, increment quantity
+    if (existingCartItem) {
+        /* Checking if the cartItem.id is equal to the productToAdd.id. If it is, it will return a new object with the
+        cartItem and the quantity + 1. If it is not, it will return the cartItem. */
+        return cartItems.map((cartItem) => cartItem.id === productToAdd.id
+            ? {...cartItem, quantity: cartItem.quantity + 1}
+            : cartItem
+        )
+    }
 
     // return new array with modified cartItems/ new cart item
+
+    return [...cartItems, {...productToAdd, quantity: 1}]
 }
 
 export const CartContext = createContext({
