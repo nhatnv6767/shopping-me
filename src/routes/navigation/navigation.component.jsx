b584b60aa2ccb972ec258439a41b6173a7660332
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-07 21:28:55 +0900

    #115 Cart Icon & Dropdown 6

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,6 +6,7 @@ import {useContext} from "react";
 import {UserContext} from "../../contexts/user.context";
 import {signOutUser} from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 export default function Navigation() {
     const {currentUser} = useContext(UserContext)
@@ -32,6 +33,7 @@ export default function Navigation() {
 
                     <CartIcon/>
                 </div>
+                <CartDropdown/>
             </div>
             <Outlet/>
         </>
