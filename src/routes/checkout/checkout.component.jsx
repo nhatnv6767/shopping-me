3b9773e2b25b6a02403537cc099d9091f60737be
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-08 00:22:44 +0900

    #123 Checkout item 1

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -5,8 +5,24 @@ import {CartContext} from "../../contexts/cart.context";
 export default function Checkout() {
     const {cartItems, addItemToCart, removeItemFromCart} = useContext(CartContext)
     return (
-        <div>
-            <h1>I am the checkout page</h1>
+        <div className="checkout-container">
+            <div className="checkout-header">
+                <div className="header-block">
+                    <span>Product</span>
+                </div>
+                <div className="header-block">
+                    <span>Description</span>
+                </div>
+                <div className="header-block">
+                    <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                    <span>Price</span>
+                </div>
+                <div className="header-block">
+                    <span>Remove</span>
+                </div>
+            </div>
             <div>
                 {
                     cartItems.map((cartItem) => {
