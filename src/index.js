0afb5a12161faa38339dd118ec924f16c885a2ba
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-07 21:41:13 +0900

    #116 Toggle cart open 2

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {UserProvider} from "./contexts/user.context";
 import {ProductsProvider} from "./contexts/products.context";
+import {CartProvider} from "./contexts/cart.context";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,7 +15,9 @@ root.render(
         <Router>
             <UserProvider>
                 <ProductsProvider>
-                    <App/>
+                    <CartProvider>
+                        <App/>
+                    </CartProvider>
                 </ProductsProvider>
             </UserProvider>
         </Router>
