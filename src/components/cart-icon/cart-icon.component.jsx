b89cbadbf82e361dccac8c204f6caf822b2922b2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-07 21:17:53 +0900

    #115 Cart Icon & Dropdown 2

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,7 +1,11 @@
 import "./cart-icon.styles.jsx.scss"
+import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg"
 
 export default function CartIcon() {
     return (
-        <div>CartIcon</div>
+        <div className="cart-icon-container">
+            <ShoppingIcon className="shopping-icon"/>
+            <span className="item-count">0</span>
+        </div>
     );
 }
