9cd3cef89b89fdac2e4c254097a6a65408e84d81
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-07 23:55:20 +0900

    #121 Creating checkout page 4

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,9 +1,25 @@
 import "./checkout.styles.scss"
+import {useContext} from "react";
+import {CartContext} from "../../contexts/cart.context";
 
 export default function Checkout() {
+    const {cartItems} = useContext(CartContext)
     return (
         <div>
             <h1>I am the checkout page</h1>
+            <div>
+                {
+                    cartItems.map((cartItem) => {
+                        const {id, name, quantity} = cartItem
+                        return (
+                            <div key={id}>
+                                <h2>{name}</h2>
+                                <span>{quantity}</span>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 }
