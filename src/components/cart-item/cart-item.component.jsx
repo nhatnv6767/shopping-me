fc88a8ced629f7e3df95c36fcf17b95f34be1ae2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-07 22:07:31 +0900

    #117 Add to Cart 1

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,12 @@
+import "./cart-item.styles.scss"
+
+
+export default function CartItem({cartItem}) {
+    const {name, quantity} = cartItem
+    return (
+        <div>
+            <h2>{name}</h2>
+            <span>{quantity}</span>
+        </div>
+    );
+}
