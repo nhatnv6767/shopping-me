9e6aebbd8c3b6bc587ac612882aee0b1c48c4483
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-08 00:56:40 +0900

    #125 Cart Total 1

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -55,6 +55,7 @@ export const CartContext = createContext({
     clearItemFromCart: () => {
     },
     cartCount: 0,
+    cartTotal: 0,
 })
 
 
@@ -62,10 +63,16 @@ export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false)
     const [cartItems, setCartItems] = useState([])
     const [cartCount, setCartCount] = useState(0)
+    const [cartTotal, setCartTotal] = useState(0)
 
     useEffect(() => {
         const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
         setCartCount(newCartCount)
+    }, [cartItems])
+
+    useEffect(() => {
+        const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0)
+        setCartTotal(newCartTotal)
     }, [cartItems])
 
     const addItemToCart = (productToAdd) => {
@@ -85,6 +92,7 @@ export const CartProvider = ({children}) => {
         cartCount,
         removeItemFromCart,
         clearItemFromCart,
+        cartTotal,
     }
     return (
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
