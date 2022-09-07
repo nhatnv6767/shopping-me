d39ec4a36cd2f63a73dfcc13b92ae8ba6f2eb5c9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-07 21:43:18 +0900

    #116 Toggle cart open 3

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,9 +7,11 @@ import {UserContext} from "../../contexts/user.context";
 import {signOutUser} from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import {CartContext} from "../../contexts/cart.context";
 
 export default function Navigation() {
     const {currentUser} = useContext(UserContext)
+    const {isCardOpen} = useContext(CartContext)
 
     return (
         <>
@@ -33,7 +35,10 @@ export default function Navigation() {
 
                     <CartIcon/>
                 </div>
-                <CartDropdown/>
+                {
+                    isCardOpen && <CartDropdown/>
+                }
+                
             </div>
             <Outlet/>
         </>
