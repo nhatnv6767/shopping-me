7e54cd722bf47096dff5db72f5648e27b1671951
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-07 21:55:40 +0900

    #116 Toggle cart open 5

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,10 +4,10 @@ import {useContext} from "react";
 import {CartContext} from "../../contexts/cart.context";
 
 export default function CartIcon() {
-    const {isCardOpen, setIsCardOpen} = useContext(CartContext)
-    const toggleIsCardOpen = () => setIsCardOpen(!isCardOpen)
+    const {isCartOpen, setIsCartOpen} = useContext(CartContext)
+    const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
     return (
-        <div className="cart-icon-container" onClick={toggleIsCardOpen}>
+        <div className="cart-icon-container" onClick={toggleIsCartOpen}>
             <ShoppingIcon className="shopping-icon"/>
             <span className="item-count">0</span>
         </div>
