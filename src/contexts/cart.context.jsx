23b9e3c1b0eb2fedafc5c1b5cae0ae55b7cff75f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-07 22:11:56 +0900

    #117 Add to Cart 2.1

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -3,8 +3,21 @@ import {createContext, useState} from "react";
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {
-    }
+    },
+    cartItems: []
 })
+
+/*
+product
+{
+id, name, price, imageUrl
+}
+
+CartItem
+{
+id, name, price, imageUrl, quantity
+}
+*/
 
 export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false)
