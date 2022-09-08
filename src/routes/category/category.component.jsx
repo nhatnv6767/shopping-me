d86d65c914569d016a353f5796ef2f8e8d4e575f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-09 02:43:55 +0900

    #134 Category page 1

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,8 @@
+import "./category.styles.scss"
+
+
+export default function Category() {
+    return (
+        <div>Category</div>
+    );
+}
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,11 +1,13 @@
 import "./shop.styles.scss"
 import {Routes, Route} from "react-router-dom"
 import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 export default function Shop() {
     return (
         <Routes>
             <Route index element={<CategoriesPreview/>}/>
+            <Route path=":category" element={<Category/>}/>
         </Routes>
     );
 }
