20b05a38e94bedb32ffa4b38c534bad44a700d09
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-09 01:22:52 +0900

    #127 AddCollectionAndDocuments <2> 2

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,6 +1,7 @@
-import {createContext, useState} from "react";
+import {createContext, useEffect, useState} from "react";
 
 import SHOP_DATA from "../shop-data.js"
+import {addCollectionAndDocuments} from "../utils/firebase/firebase.utils";
 
 export const ProductsContext = createContext({
     products: [],
@@ -8,6 +9,12 @@ export const ProductsContext = createContext({
 
 export const ProductsProvider = ({children}) => {
     const [products, setProducts] = useState([])
+    useEffect(() => {
+        const initial = async () => {
+            await addCollectionAndDocuments("categories", SHOP_DATA)
+        }
+        initial()
+    }, [])
     /* Creating a new object with the key of products and the value of products. */
     const value = {products}
     return (
