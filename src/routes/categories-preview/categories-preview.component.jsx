c85557d4aa22ede72794229ce3a24a5f286ca059
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-09 02:30:57 +0900

    #133 Nested Routes in Shop 2

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,1 +1,24 @@
 import "./categories-preview.styles.scss"
+import {useContext} from "react";
+import {CategoriesContext} from "../../contexts/categories.context";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+
+export default function CategoriesPreview() {
+    const {categoriesMap} = useContext(CategoriesContext)
+    return (
+        <div className="category-preview-container">
+
+            {
+                // because it is an object
+                Object.keys(categoriesMap).map(title => {
+                    const products = categoriesMap[title]
+                    return (
+                        <CategoryPreview key={title} title={title} products={products}/>
+                    )
+                })
+            }
+
+
+        </div>
+    );
+}
