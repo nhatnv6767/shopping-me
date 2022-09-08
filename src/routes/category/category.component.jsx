38f8c425a284829955745716cc36496e4a9c0ae5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-09 02:53:49 +0900

    #134 Category page 3.3

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -8,7 +8,7 @@ import ProductCard from "../../components/product-card/product-card.component";
 export default function Category() {
     const {category} = useParams()
     const {categoriesMap} = useContext(CategoriesContext)
-    const [products, setProducts] = useState()
+    const [products, setProducts] = useState(categoriesMap[category])
 
     useEffect(() => {
         setProducts(categoriesMap[category])
