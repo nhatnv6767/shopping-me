b46c92445c4dffd0c649454c459036637c979b7d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-09 02:45:56 +0900

    #134 Category page 2

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,7 +1,13 @@
 import "./category.styles.scss"
+import {useParams} from "react-router-dom";
+import {useContext} from "react";
+import {CategoriesContext} from "../../contexts/categories.context";
 
 
 export default function Category() {
+    const {category} = useParams()
+    const {categoriesMap} = useContext(CategoriesContext)
+    const products = categoriesMap[category]
     return (
         <div>Category</div>
     );
