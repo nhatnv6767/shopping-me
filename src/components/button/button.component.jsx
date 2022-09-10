0a10c69b17fe6e2aa5e2a214fed1b39cb57786f3
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-10 12:52:21 +0900

    #137 Styled components - Button 2

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -6,12 +6,21 @@ inverted
 google sign in
 
  */
-import "./button.styles"
+import {BaseButton, GoogleSignInButton, InvertedButton} from "./button.styles"
 
 const BUTTON_TYPES_CLASSES = {
+    base: "base",
     google: "google-sign-in",
     inverted: "inverted"
 }
+
+const getButton = (buttonType = BUTTON_TYPES_CLASSES.base) => (
+    {
+        [BUTTON_TYPES_CLASSES.base]: BaseButton,
+        [BUTTON_TYPES_CLASSES.google]: GoogleSignInButton,
+        [BUTTON_TYPES_CLASSES.inverted]: InvertedButton
+    }[buttonType]
+)
 
 export default function Button({children, buttonType, ...otherProps}) {
     return (
