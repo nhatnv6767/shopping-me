4720033f206d2260f02cd077ddde10c44af13402
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-10 23:33:59 +0900

    #139 Styled components - Directory item & cart icon 1

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,4 +1,4 @@
-import "./cart-icon.styles.jsx.scss"
+import "./cart-icon.styles.jsx"
 import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg"
 import {useContext} from "react";
 import {CartContext} from "../../contexts/cart.context";
