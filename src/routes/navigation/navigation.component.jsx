ed4b29e7d0fb1ad4aef4c5193e6a7675b8291ab0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-10 10:05:36 +0900

    #136 Styled components 5

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,7 +7,12 @@ import {signOutUser} from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import {CartContext} from "../../contexts/cart.context";
-import {NavigationContainer} from "./navigation.styles"
+import {
+    NavigationContainer,
+    LogoContainer,
+    NavLinks,
+    NavLink,
+} from "./navigation.styles"
 
 export default function Navigation() {
     const {currentUser} = useContext(UserContext)
@@ -16,25 +21,25 @@ export default function Navigation() {
     return (
         <>
             <NavigationContainer>
-                <Link className="logo-container" to="/">
+                <LogoContainer to="/">
                     <CrwnLogo className="logo"/>
-                </Link>
-                <div className="nav-links-container">
-                    <Link to="/shop" className="nav-link">
+                </LogoContainer>
+                <NavLinks>
+                    <NavLink to="/shop">
                         SHOP
-                    </Link>
+                    </NavLink>
                     {
                         currentUser ? (
                             <span className="nav-link" onClick={signOutUser}>SIGN OUT</span>
                         ) : (
-                            <Link to="/auth" className="nav-link">
+                            <NavLink to="/auth">
                                 SIGN IN
-                            </Link>
+                            </NavLink>
                         )
                     }
 
                     <CartIcon/>
-                </div>
+                </NavLinks>
                 {
                     isCartOpen && <CartDropdown/>
                 }
