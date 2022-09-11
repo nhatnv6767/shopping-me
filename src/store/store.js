f80423df71481d66fd742b349c35295f73f1633f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-11 19:21:59 +0900

    #152 Redux - Setting up store 1

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import {compose, createStore, applyMiddleware} from "redux";
 import logger from "redux-logger";
+import {rootReducer} from "./root-reducer";
 
 // root-reducer
+export const store = createStore(rootReducer)
