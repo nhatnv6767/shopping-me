80aed78b8d92c7c6d3dad53eb1bd2785e60e10c0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-11 23:46:45 +0900

    #156 Categories selector 5

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,8 +1,13 @@
-import {Fragment, useContext} from "react";
+import {Fragment} from "react";
 import CategoryPreview from "../../components/category-preview/category-preview.component";
+import {useSelector} from "react-redux";
+import {selectCategoriesMap} from "../../store/categories/category.selector";
+
 
 export default function CategoriesPreview() {
-    const {categoriesMap} = useContext(CategoriesContext)
+
+    const categoriesMap = useSelector(selectCategoriesMap)
+
     return (
         <Fragment>
 
