33bb93f1d1112e052679dce01790e5f0d82be30b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-11 23:10:53 +0900

    #000 Try to fix redux-logger error TEMP

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,10 @@
-import {compose, createStore, applyMiddleware} from "redux";
-import logger from "redux-logger";
+import {createStore, applyMiddleware} from "redux";
 import {rootReducer} from "./root-reducer";
 import {composeWithDevTools} from "redux-devtools-extension";
-import thunk from "redux-thunk";
 
-// run after action hit reducer
-// const middleWares = [logger]
-// const composedEnhancers = compose(applyMiddleware(...middleWares))
-// export const store = createStore(rootReducer, undefined, composedEnhancers)
-
+const middleware = []
 export const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(thunk))
+    undefined,
+    composeWithDevTools(applyMiddleware(...middleware))
 )
