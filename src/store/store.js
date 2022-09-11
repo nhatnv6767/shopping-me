1a249b5bbf03b773d5e836b2dec5106b9e16e733
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-11 23:03:49 +0900

    #000 Try to fix redux-logger error 4

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,15 @@
 import {compose, createStore, applyMiddleware} from "redux";
 import logger from "redux-logger";
 import {rootReducer} from "./root-reducer";
-import {configureStore} from '@reduxjs/toolkit'
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
 // run after action hit reducer
 // const middleWares = [logger]
 // const composedEnhancers = compose(applyMiddleware(...middleWares))
 // export const store = createStore(rootReducer, undefined, composedEnhancers)
 
-export const store = configureStore({
-    reducer: rootReducer,
-})
+export const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+)
--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -2,5 +2,9 @@ import {createAction} from "../../utils/reducer/reducer.utils";
 import {USER_ACTION_TYPES} from "./user.types";
 
 export const setCurrentUser = (user) => {
-    createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user)
+    return {
+        type: USER_ACTION_TYPES.SET_CURRENT_USER,
+        user,
+    }
+    // createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user)
 }
