60d0190460bd2eb612a50f927399081b14996e6d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-11 23:34:09 +0900

    #156 Categories selector 1

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,8 +2,17 @@ import "./shop.styles.scss"
 import {Routes, Route} from "react-router-dom"
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
+import {useEffect} from "react";
+import {getCategoriesAndDocuments} from "../../utils/firebase/firebase.utils";
 
 export default function Shop() {
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments()
+            setCategoriesMap(categoryMap)
+        }
+        getCategoriesMap()
+    }, [])
     return (
         <Routes>
             <Route index element={<CategoriesPreview/>}/>
