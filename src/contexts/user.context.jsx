9a4a854fc196188738c5ff2f732c420aaae3180b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-11 16:13:35 +0900

    #145 User reducer 7.1

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -13,6 +13,7 @@ export const USER_ACTION_TYPES = {
 
 
 const userReducer = (state, action) => {
+    console.log("userReducer action: ", action)
     const {type, payload} = action;
 
     switch (type) {
