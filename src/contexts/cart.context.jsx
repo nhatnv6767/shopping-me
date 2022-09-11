2feb436f6cc3880c7e25f051e795332f76497ba1
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-11 16:44:53 +0900

    #146 Cart reducer<1> 6

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import {createContext, useEffect, useState} from "react";
+import {createContext, useEffect, useReducer, useState} from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
     // find if cartItems contains productToAdd
@@ -68,6 +68,11 @@ const INITIAL_STATE = {
 const cartReducer = (state, action) => {
     const {type, payload} = action;
     switch (type) {
+        case 'SET_CART_ITEMS':
+            return {
+                ...state,
+                ...payload,
+            }
         default:
             throw new Error(`Unhandled type of ${type} in cartReducer`)
     }
@@ -75,21 +80,22 @@ const cartReducer = (state, action) => {
 
 
 export const CartProvider = ({children}) => {
-    const [isCartOpen, setIsCartOpen] = useState(false)
-    const [cartItems, setCartItems] = useState([])
-    const [cartCount, setCartCount] = useState(0)
-    const [cartTotal, setCartTotal] = useState(0)
+    // const [isCartOpen, setIsCartOpen] = useState(false)
+    // const [cartItems, setCartItems] = useState([])
+    // const [cartCount, setCartCount] = useState(0)
+    // const [cartTotal, setCartTotal] = useState(0)
+    //
+    // useEffect(() => {
+    //     const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
+    //     setCartCount(newCartCount)
+    // }, [cartItems])
+    //
+    // useEffect(() => {
+    //     const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0)
+    //     setCartTotal(newCartTotal)
+    // }, [cartItems])
 
-    useEffect(() => {
-        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
-        setCartCount(newCartCount)
-    }, [cartItems])
-
-    useEffect(() => {
-        const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0)
-        setCartTotal(newCartTotal)
-    }, [cartItems])
-
+    const [state, dispatch] = useReducer()
     const updateCartItemsReducer = (newCartItems) => {
         const newCartCount = newCartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
         const newCartTotal = newCartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0)
