f4f428c8b4472e1fd6ce2039759439e6465fccbc
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-12 22:30:43 +0900

    #159 Demystifying middleware 1

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,23 @@
-import {createStore, applyMiddleware} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
+import thunkMiddleware from 'redux-thunk'
 import {rootReducer} from "./root-reducer";
 import {composeWithDevTools} from "redux-devtools-extension";
+import loggerMiddleware from "../utils/middleware/logger";
 
-const middleware = []
-export const store = createStore(
-    rootReducer,
-    undefined,
-    composeWithDevTools(applyMiddleware(...middleware))
-)
+import {configureStore} from "@reduxjs/toolkit";
+import monitorReducerEnhancer from "../utils/enhancers/monitorReducer";
+
+
+const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunkMiddleware)
+const composedEnhancers = compose(middlewareEnhancer, monitorReducerEnhancer)
+
+export const store = createStore(rootReducer, undefined, composedEnhancers)
+
+// export const store = configureStore(
+//     {
+//         reducer: rootReducer,
+//         composedEnhancers
+//     }
+// )
+
+
